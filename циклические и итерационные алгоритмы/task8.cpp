3c7078d1a4548c3f8e043b06c47aa3c5c6092302
the-are-light <artemrashu7@gmail.com>
2024-10-14 17:58:39 +0800

    Update task8.cpp

--- циклические и итерационные алгоритмы/task8.cpp
+++ циклические и итерационные алгоритмы/task8.cpp
@@ -3,7 +3,7 @@
 #include <math.h>
 
 void main() {
-	float e, n = 1.0;
+	double e, n = 1.0;
 	std::cout << "Enter inaccuracy: "; std::cin >> e;
 	while (M_E - pow(1 + 1 / n, n) >= e) n++;
 	std::cout << "The expression under study differs from M_E by a inaccuracy " << e << " at n = " << n << std::endl << "Result: " << M_E - pow(1 + 1 / n, n);
