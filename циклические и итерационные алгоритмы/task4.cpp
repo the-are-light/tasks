3278c153f1e31f14c848b253df7e7566f4a08929
the-are-light <artemrashu7@gmail.com>
2024-10-16 19:38:41 +0800

    Update task4.cpp

--- циклические и итерационные алгоритмы/task4.cpp
+++ циклические и итерационные алгоритмы/task4.cpp
@@ -3,9 +3,21 @@
 void main()
 {
     //Коммерция
+    setlocale(LC_ALL, "Russian");
     double k, p, r;
-    std::cout << "Enter the loan amount, the percentage of annual payments in decimal format, and the annual profit: "; std::cin >> k >> p >> r;
-    if (r <= k * p) std::cout << "The loan cannot be repaid";
-    else std::cout <<"You will be able to repay the loan in "<< k / (r - k * p) << " years" ;
+    std::cout << "Введите сумму кредита: "; std::cin >> k;
+    std::cout << "Введите процентную ставку в десятичном формате: "; std::cin >> p;
+    std::cout << "Введите вашу годовую прибыль: "; std::cin >> r;
+    
+    double total = k * (1 + p); // общая сумма кредита
+    double remain = total - r; //кредит после первого года
+    int years = 1;
+    
+    while (remain > 0) {
+        remain -= r; years++;
+    }
+
+    if (years > 0) std::cout << "Кредит будет погашен через " << years << " лет";
+    else std::cout << "Кредит выплачен";
 
 }
